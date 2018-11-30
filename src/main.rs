992fb1826c01ed32cd2ccb4ed59699928bbb0eea
naens <naens@gmx.com>
2018-11-30 15:16:03 +0100

    add readline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+extern crate rustyline;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+mod control; 
+mod textbuf;
+use control::Control;
+
 fn main() {
-    println!("Hello, world!");
+    let mut r1 = Editor::<()>::new();
+    let control = Control::new();
+    loop {
+        let prompt = control.get_prompt();
+        let readline = r1.readline(prompt);
+        match readline {
+            Ok(line) => {
+                control.execute(&line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("INT");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("EOF");
+                break;
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
 }
