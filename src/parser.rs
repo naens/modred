26afa5d378f67201ac599e3fb839690a2d05a2ad
naens <naens@gmx.com>
2018-12-12 17:34:35 +0100

    fix lifetimes, define Command enum

--- src/parser.rs
+++ src/parser.rs
@@ -4,6 +4,12 @@ struct Parser<'a> {
 }
 
 enum CommandId {
+    V,A,W,E,					// 4.3
+    B,C,L,Number,T,D,K,I,S,			// 4.4
+    P,LineNumber, ThroughLineNumber,		// 4.6.1
+    F,N,J,M,Z,					// 4.6.2
+    X,R,					// 4.6.3
+    H,O,Q					// 4.6.4
 }
 
 struct Command<'a> {
@@ -12,9 +18,19 @@ struct Command<'a> {
     tail: &'a str
 }
 
+impl<'a> Parser<'a> {
+    fn new(text: &str) -> Parser {
+        Parser {
+            text: text,
+            index: 0
+        }
+    }
+}
+
 impl<'a> Iterator for Parser<'a> {
     type Item = Command<'a>;
 
-    fn next(&mut self) -> Option<Command> {
+    fn next(&mut self) -> Option<Command<'a>> {
+        None
     }
 }
