889e6b9bc9c3845231c247555ebe8210570c8811
naens <naens@gmx.com>
2020-05-27 23:23:43 +0200

    data: update

--- data.fs
+++ data.fs
@@ -2,19 +2,18 @@
 
 \ Single line
 struct
-   cell% field line-next		\ next line
    cell% field line-prev		\ previous line
-   cell% field text			\ link to the contents of the line
+   cell% field line-next		\ next line
+   cell% field line-text		\ link to the contents of the line
 end-struct line%
 
 \ The buffer
 struct
-   cell% field buffer-line		\ line number
-   cell% field buffer-column		\ column number
-   cell% field buffer-name		\ buffer display name
-   cell% field buffer-filename		\ name of the associated file
-   cell% field buffer-first-line	\ link to the first line
-   char% field buffer-modified		\ modified flag
-   char% field buffer-readonly		\ buffer is read-only flag
-   char% field buffer-draft		\ buffer is a draft flag
-end-struct buffer%
+   cell% field text-buffer-line		\ line number
+   cell% field text-buffer-name		\ buffer display name
+   cell% field text-buffer-filename	\ name of the associated file
+   cell% field text-buffer-first-line	\ link to the first line
+   char% field text-buffer-modified	\ modified flag
+   char% field text-buffer-readonly	\ buffer is read-only flag
+   char% field text-buffer-draft	\ buffer is a draft flag
+end-struct text-buffer%
