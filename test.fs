cb2495d8a5aca6b4731ff7e27057f1322e6a51de
naens <naens@gmx.com>
2020-06-11 23:09:00 +0200

    test: display text after edit

--- test.fs
+++ test.fs
@@ -3,6 +3,17 @@
 include data.fs
 include do-edit.fs
 
+\ display the text
+: display-text ( first-line% -- )
+   0 over ( line% counter line% )
+   begin
+      0<> while ( line% counter )
+      dup . ." '"
+      over line-text count type ." '" cr
+      1+ swap
+      line-next @ tuck
+   repeat ;
+
 \ save terminal state
 s" stty -g" r/o open-pipe throw slurp-fid 2constant terminal-state
 
@@ -12,9 +23,12 @@ s" stty intr undef; stty susp undef" system
 \ test editing functionality
 s" 0123456789" do-edit
 
+cr ." Line offset: " . cr
+." Text: " cr
+display-text
+." ---END---"
+
 \ restore terminal
 s" stty " terminal-state s+ system
 
 quit
-\ page
-
