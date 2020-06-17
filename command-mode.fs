8d13b56b907a183e48b6e0534a9e64f8f4608c42
naens <naens@gmx.com>
2020-06-17 23:44:39 +0200

    command-mode: initialize

---
+++ command-mode.fs
@@ -0,0 +1,8 @@
+\ ****h* Modred/Command Mode
+\ FUNCTION
+\   Display and navigate the current buffer
+\   Enter Insert Mode
+\   Search, replace in the current buffer
+\   Copy, paste, delete lines
+\   Open, save, close, files
+\ ******
