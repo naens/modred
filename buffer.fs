5afba97891769ef7deab3ab66170776ec4e93e86
naens <naens@gmx.com>
2020-06-20 23:50:19 +0200

    clarify issues with data

---
+++ buffer.fs
@@ -0,0 +1,18 @@
+\ ****h* modred/buffer
+\ FUNCTION
+\    The Buffer module contains words for the management of
+\    Buffer objects.  That is the creation, destruction, data,
+\    and cursor position.
+\    The words are meant to be re-entrant.  It contains words
+\    for creation and destruction, but no data is stored in the
+\    module.
+\ ******
+
+\ TODO: creation and initialization words
+
+\ TODO: insertion words
+
+\ TODO: deletion words
+
+\ TODO: navigation words
+
