c7c4645c0811fa2f4d6c2cb978df57a2f178a1e4
naens <naens@gmx.com>
2020-06-24 23:29:22 +0200

    buffer: data and buffer in one module

--- buffer.fs
+++ buffer.fs
@@ -13,6 +13,42 @@
 \ ******
 
 
+\ data structures for the buffer
+
+\ ****s* buffer/buffer-line%
+\ FUNCTION
+\   Represents a node of a doubly linked list of the buffer.
+\   * prev: pointer to the previous line
+\   * next: pointer to the next line
+\   * text: pointer to the string
+\ ******
+struct
+   cell% field buffer-line-prev		\ previous line
+   cell% field buffer-line-next		\ next line
+   cell% field biffer-line-text		\ link to the contents of the line
+end-struct buffer-line%
+
+\ ****s* buffer/buffer%
+\ FUNCTION
+\   Represents the buffer with its properties and content.
+\   * line: line number
+\   * name: buffer name
+\   * filename: name of the associated file
+\   * first-line: link to the first line
+\   * modified: modified flag
+\   * buffer-readonly: buffer is read-only flag
+\   * draft: buffer is a draft flag
+\ ******
+struct
+   cell% field buffer-line
+   cell% field buffer-name
+   cell% field buffer-filename
+   buffer-line% field buffer-first-line
+   char% field buffer-modified
+   char% field buffer-readonly
+   char% field buffer-draft
+end-struct buffer%
+
 \ TODO: creation and initialization words
 
 \ create new
