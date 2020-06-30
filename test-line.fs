bb76f49a44362802f46ece5479e0193e69447a36
naens <naens@gmx.com>
2020-06-30 23:23:39 +0200

    test-line.fs: set-string tests

--- test-line.fs
+++ test-line.fs
@@ -23,12 +23,26 @@ include line.fs
 \ TODO:    -> test when new capacity > current + line-chunk-size
 \ TODO:    -> test when new capacity > 255 (illegal string)
 
-: line-set-string-test ( addr u -- bool )
-   line-new                     ( addr u line% )
-   dup 2over line-set-string    ( addr u line% )
-   dup line-get-string          ( addr u line% string )
-   swap line-delete count       ( aadr u addr1 u1 )
-   str= ;
+: line-set-string-test ( addr u cap -- bool )
+   line-new                     ( addr u cap line% )
+   dup s" abcd" line-set-string ( addr u cap line% )
+   dup 2over line-set-string    ( addr u cap line% )
+   dup line-get-capacity        ( addr u cap line% cap2 )
+   rot = if                     ( addr u line% )
+      dup line-get-string       ( addr u line% string )
+      swap line-delete count    ( aadr u addr1 u1 )
+      str=
+   else                         ( addr u line% )
+      drop 2drop false
+   then ;
+
+: line-set-string-tests
+   s" " line-chunk-size line-set-string-test
+   s" 123" line-chunk-size line-set-string-test
+   s" prst" line-chunk-size line-set-string-test
+   s" 0123456789ABCDEF123" line-chunk-size 2 * line-set-string-test
+   s" 0123456789ABCDEFoprst" line-chunk-size 2 * line-set-string-test
+   ;
    
 : test-line \ run all tests
    line-new-test if ." line-new-test fail" cr then
